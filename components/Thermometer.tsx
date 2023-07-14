4b62572c23aa3c309c58f75f2a9e74fb617bcb9f
pyTimK <45229539+pyTimK@users.noreply.github.com>
2023-07-14 23:33:11 +0800

    updated thermometer

--- components/Thermometer.tsx
+++ components/Thermometer.tsx
@@ -19,41 +19,54 @@ const Thermometer: React.FC<ThermometerProps> = ({ temp }) => {
   return (
     <div className="relative">
       <svg
-        width="96"
-        height="324"
-        viewBox="0 0 96 324"
+        width="171"
+        height="402"
+        viewBox="0 0 171 402"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
       >
-        <circle cx="48" cy="276.822" r="47.1777" fill="#D9D9D9" />
+        <circle cx="86.5889" cy="342.705" r="58.5889" fill="#D9D9D9" />
         <rect
-          x="24.0879"
-          y="0.864868"
-          width="47.824"
-          height="260.447"
-          rx="23.912"
+          x="56.8931"
+          width="59.3914"
+          height="323.443"
+          rx="29.6957"
           fill="#D9D9D9"
         />
         <rect
-          x="33.782"
-          y="25.4231"
-          width="28.4359"
-          height="264.971"
-          rx="14.2179"
+          x="68.9319"
+          y="30.4983"
+          width="35.3138"
+          height="329.061"
+          rx="17.6569"
           fill="#BFBFBF"
         />
-        <circle cx="48" cy="276.822" r="34.8986" fill={`${"#EB2437"}`} />
+        <circle cx="86.589" cy="342.705" r="43.3397" fill="#EB2437" />
         <rect
-          x="33.782"
-          y={`${260 - tempHeight}`}
-          width="28.4359"
+          x="68.9319"
+          y={`${323 - tempHeight}`}
+          width="35.3138"
           height={`${tempHeight}`}
-          rx="14.2179"
-          fill={`${"#EB2437"}`}
+          rx="17.6569"
+          fill="#EB2437"
         />
+
+        <rect x="116" y="30" width="55" height="4" rx="2" fill="#EB2437" />
+        <rect x="116" y="59" width="36.8902" height="4" rx="2" fill="#23A9E7" />
+        <rect x="116" y="88" width="55" height="4" rx="2" fill="white" />
+        <rect
+          x="116"
+          y="117"
+          width="36.8902"
+          height="4"
+          rx="2"
+          fill="#44C8F9"
+        />
+        <rect x="116" y="146" width="55" height="4" rx="2" fill="#EB2437" />
       </svg>
-      <div className="flex flex-row items-start justify-center absolute inset-x-0 bottom-8">
-        <p className={`${skyerFont.className} text-2xl`}>{temp}</p>
+
+      <div className="flex flex-row items-start justify-center absolute inset-x-0 bottom-10">
+        <p className={`${skyerFont.className} text-4xl`}>{temp}</p>
         <p className={`${skyerFont.className} text-sm`}>°C</p>
       </div>
     </div>
